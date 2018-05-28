f4e91c63e61012220ac98efc38b170237ed6c62f
Alfredo Miranda <alfredo@miranda.io>
2018-05-28 15:40:01 -0500

    Prevent window from navigating away when dropping a file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import './stylesheets/styles.css';
 
+window.addEventListener('drop', e => {
+  e.preventDefault();
+});
+
+window.addEventListener('dragover', e => {
+  e.preventDefault();
+});
+
 const aspirin = 'O=C(Oc1ccccc1C(=O)O)C 0';
 console.log('hey there');
