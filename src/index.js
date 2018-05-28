0d60cb5a30507f4bbb7f450153f754a460335384
Alfredo Miranda <alfredo@miranda.io>
2018-05-28 17:53:32 -0500

    Read file, validate it, and print it to the console

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import smiles from 'smiles';
 import './stylesheets/styles.css';
 
 const dropArea = document.querySelector('.drop-area');
@@ -15,3 +16,32 @@ dropArea.addEventListener('dragleave', function(event) {
   this.classList.remove('full');
   this.classList.add('empty');
 });
+
+dropArea.addEventListener('drop', event => {
+  readFileContents(event.dataTransfer.files[0]).then(
+    fileContents => {
+      console.log(fileContents);
+    },
+    e => {
+      console.log('error', e);
+    }
+  );
+});
+
+function readFileContents(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+      const smilesString = event.target.result.replace('\n', '');
+
+      try {
+        smiles.parse(smilesString);
+        resolve(smilesString);
+      } catch (e) {
+        reject(e);
+      }
+    };
+    reader.readAsText(file);
+  });
+}
